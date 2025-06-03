460ad1ec78906940cb4edc48548842e20b5ba71e
Fernando Satti <fernandoist98@gmail.com>
2025-06-03 13:40:07 -0300

    moficiacion

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
         {/* Botón fijo en la parte superior derecha */}
         <a
           href="https://alfonsa-tools-modern.vercel.app/" // ⬅️ Cambiá por tu URL real
-          target="_blank"
           rel="noopener noreferrer"
           className="fixed top-4 right-4 z-50 px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition"
         >
